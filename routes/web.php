e3f011eba41891063a8b7b2e8779adfe88fa76a6
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-09-08 01:46:18 +0545

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ObsController;
 
-Route::view('/', 'control');
+Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+// Route::view('profile', 'profile')
+//     ->middleware(['auth'])
+//     ->name('profile');
 
 Route::get('/start-streaming', [ObsController::class, 'startStreaming']);
 Route::get('/stop-streaming', [ObsController::class, 'stopStreaming']);
