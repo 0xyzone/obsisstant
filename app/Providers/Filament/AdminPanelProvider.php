256f396b2692448682e509fa8b83fd53debcbbbb
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-09-10 02:56:16 +0545

    added favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->id('admin')
             ->path('admin')
+            ->favicon(asset('ObsistanT.png'))
             ->login()
             ->colors([
                 'primary' => Color::Violet,
