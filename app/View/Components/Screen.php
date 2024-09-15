74e5dc8b09b76c1288dde80fabe5b4eb756f060d
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-09-15 07:03:53 +0545

    asdfasdf

--- app/View/Components/Screen.php
+++ app/View/Components/Screen.php
@@ -5,7 +5,7 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class ScreenLayout extends Component
+class Screen extends Component
 {
     /**
      * Get the view / contents that represents the component.
