469115e9b1bf96b0e43b51e471b9b5c885070d6f
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-09-26 03:52:20 +0545

    update

--- resources/views/infolists/components/team-arooster.blade.php
+++ resources/views/infolists/components/team-arooster.blade.php
@@ -67,8 +67,8 @@
                     @endphp
                     <div class="flex gap-4 bg-gray-800/50">
                         <div class="relative">
-                            <img src=" {{ App\Models\Hero::where('name', $playerData->hero)->first()->hero_image_url ?? "" }}" class="w-32" alt="{{ $playerData->hero }}">
-                            <p class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full text-center" style="background-color: {{ $getRecord()->tournament->themes[0]->primary_color }};">{{ $playerData->hero }}</p>
+                            <img src=" {{ App\Models\Hero::where('id', $playerData->hero)->first()->hero_image_url ?? "" }}" class="w-32" alt="{{ App\Models\Hero::where('id', $playerData->hero)->first()->name }}">
+                            <p class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full text-center" style="background-color: {{ $getRecord()->tournament->themes[0]->primary_color }};">{{ App\Models\Hero::where('id', $playerData->hero)->first()->name }}</p>
                             @if ($playerData->is_mvp == true)
                                 <p class="absolute top-0 right-0 px-2 bg-amber-400 text-gray-900 text-xs font-bold">MVP</p>
                             @endif
